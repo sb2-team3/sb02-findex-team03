74d8b5f39b3175be32966440abbbd4fac6306e62
leejunnyeop <daffodil8620@naver.com>
2025-04-21 15:53:43 +0900

    [feat] 자동연동 엔티티 구현

---
+++ src/main/java/com/findex/demo/autoSyncConfig/domain/entity/AutoSyncConfig.java
@@ -0,0 +1,20 @@
+package com.findex.demo.autoSyncConfig.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class AutoSyncConfig {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Boolean enabled;
+}
