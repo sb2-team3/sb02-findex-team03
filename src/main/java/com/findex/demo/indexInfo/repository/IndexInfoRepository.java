5fee6ed22c778fce84c9a17c878d3cd3d9b67013
nayu-yuna <dldldbsk@naver.com>
2025-04-21 17:12:05 +0900

    IndexInfo repository 구현

---
+++ src/main/java/com/findex/demo/indexInfo/repository/IndexInfoRepository.java
@@ -0,0 +1,7 @@
+package com.findex.demo.indexInfo.repository;
+
+import com.findex.demo.indexInfo.domain.entity.IndexInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IndexInfoRepository extends JpaRepository<IndexInfo, Long> {
+}
