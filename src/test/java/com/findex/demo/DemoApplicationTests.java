de3140701279082fb495c6228d078fd59d46659d
leejunnyeop <daffodil8620@naver.com>
2025-04-21 14:51:52 +0900

    [init] 초기세팅

---
+++ src/test/java/com/findex/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.findex.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
