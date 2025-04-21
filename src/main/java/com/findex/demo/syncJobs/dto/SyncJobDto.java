c5a7d9bdbf4c3acde73508047bf0561acb995f00
KimSeeun <9035anny@naver.com>
2025-04-21 21:00:47 +0900

    refactor : enum 구현 type 패키지로 이동

--- src/main/java/com/findex/demo/syncJobs/dto/SyncJobDto.java
+++ src/main/java/com/findex/demo/syncJobs/dto/SyncJobDto.java
@@ -1,5 +1,25 @@
 package com.findex.demo.syncJobs.dto;
 
-public class SyncJobDto {
+import com.findex.demo.syncJobs.type.JobType;
+import com.findex.demo.syncJobs.type.Result;
+import java.time.Instant;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SyncJobDto {
+  private Long id;
+  private JobType jobType;
+  private Long indexInfoId;
+  private Instant targetDate;
+  private String worker;
+  private Instant jobTime;
+  private Result result;
 }
