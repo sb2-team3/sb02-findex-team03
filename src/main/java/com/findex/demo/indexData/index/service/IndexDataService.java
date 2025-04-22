9665c386f9c7c8412ed6967c61d5247cc72c6f48
leejunnyeop <daffodil8620@naver.com>
2025-04-22 11:53:59 +0900

    fix : IndexDataService 주석 처리

--- src/main/java/com/findex/demo/indexData/index/service/IndexDataService.java
+++ src/main/java/com/findex/demo/indexData/index/service/IndexDataService.java
@@ -1,12 +1,5 @@
 package com.findex.demo.indexData.index.service;
 
-import com.findex.demo.global.error.CustomException;
-import com.findex.demo.global.error.ErrorCode;
-import com.findex.demo.indexData.index.domain.dto.IndexDataCreateRequest;
-import com.findex.demo.indexData.index.domain.entity.IndexData;
-import com.findex.demo.indexData.index.repository.IndexDataRepository;
-import com.findex.demo.indexInfo.domain.entity.IndexInfo;
-import com.findex.demo.indexData.index.mapper.IndexDataMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,24 +7,24 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class IndexDataService {
 
-  private final IndexInfoRepository indexInfoRepository;
-  private final IndexDataRepository indexDataRepository;
-
-  public void registerIndexData(IndexDataCreateRequest request) {
-
-    // 1. 해당 지수 정보 존재 여부 확인
-    IndexInfo indexInfo = indexInfoRepository.findById(request.getIndexInfoId())
-        .orElseThrow(() -> new IllegalArgumentException("지수 정보가 존재하지 않습니다."));
-
-    // 2. 중복 확인
-    boolean exists = indexDataRepository.existsByIndexInfoAndDate(indexInfo, request.getBaseDate());
-    if (exists) {
-      throw new CustomException(ErrorCode.VALIDATION_ERROR,"이미 해당 날짜에 지수 데이터가 등록되어 있습니다.");
-    }
-
-    // 3. 등록
-    IndexDataMapper mapper = new IndexDataMapper();
-    IndexData data = mapper.toEntity(request);
-    indexDataRepository.save(data);
-  }
+//  private final IndexInfoRepository indexInfoRepository;
+//  private final IndexDataRepository indexDataRepository;
+//
+//  public void registerIndexData(IndexDataCreateRequest request) {
+//
+//    // 1. 해당 지수 정보 존재 여부 확인
+//    IndexInfo indexInfo = indexInfoRepository.findById(request.getIndexInfoId())
+//        .orElseThrow(() -> new IllegalArgumentException("지수 정보가 존재하지 않습니다."));
+//
+//    // 2. 중복 확인
+//    boolean exists = indexDataRepository.existsByIndexInfoAndDate(indexInfo, request.getBaseDate());
+//    if (exists) {
+//      throw new CustomException(ErrorCode.VALIDATION_ERROR,"이미 해당 날짜에 지수 데이터가 등록되어 있습니다.");
+//    }
+//
+//    // 3. 등록
+//    IndexDataMapper mapper = new IndexDataMapper();
+//    IndexData data = mapper.toEntity(request);
+//    indexDataRepository.save(data);
+//  }
 }
