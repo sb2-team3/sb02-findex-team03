d283ce6efc53cf679c0d8031ff1a3c536b8b4bac
leejunnyeop <daffodil8620@naver.com>
2025-04-22 20:41:01 +0900

    style: 코드 정리 및 불필요한 import 제거

--- src/main/java/com/findex/demo/indexData/index/repository/IndexDataRepository.java
+++ src/main/java/com/findex/demo/indexData/index/repository/IndexDataRepository.java
@@ -1,14 +1,8 @@
 package com.findex.demo.indexData.index.repository;
 
-import com.findex.demo.indexData.index.domain.dto.IndexDataSearchCondition;
 import com.findex.demo.indexData.index.domain.entity.IndexData;
 import com.findex.demo.indexInfo.domain.entity.IndexInfo;
-import java.awt.print.Pageable;
 import java.time.LocalDate;
-import java.util.List;
-import java.util.UUID;
-import org.springframework.data.domain.Page;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
