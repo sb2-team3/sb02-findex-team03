02ff0e6bce1784949e641aedaf684092247015f8
nayu-yuna <dldldbsk@naver.com>
2025-04-22 11:34:06 +0900

    IndexInfo Service 구현

--- src/main/java/com/findex/demo/indexInfo/repository/IndexInfoRepository.java
+++ src/main/java/com/findex/demo/indexInfo/repository/IndexInfoRepository.java
@@ -4,4 +4,5 @@ import com.findex.demo.indexInfo.domain.entity.IndexInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IndexInfoRepository extends JpaRepository<IndexInfo, Long> {
+  boolean existsByIndexClassificationAndIndexName(String indexClassification, String indexName);
 }
