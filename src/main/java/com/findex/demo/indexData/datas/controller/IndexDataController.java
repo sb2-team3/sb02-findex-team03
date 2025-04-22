bcfc1a26ea58cf00352ceddaf737c3da39130d06
kitmtaewoo <bezzi1654@kookmin.ac.kr>
2025-04-22 11:53:23 +0900

    feature: 지수데이터 API controller 구현(지수차트조회, 지수성과랭킹조회, 관심지수성조회

---
+++ src/main/java/com/findex/demo/indexData/datas/controller/IndexDataController.java
@@ -0,0 +1,55 @@
+package com.findex.demo.indexData.datas.controller;
+
+import com.findex.demo.indexData.datas.domain.dto.IndexChartDto;
+import com.findex.demo.indexData.datas.domain.dto.IndexPerformanceDto;
+import com.findex.demo.indexData.datas.domain.dto.PeriodType;
+import com.findex.demo.indexData.datas.domain.dto.RankedIndexPerformanceDto;
+import com.findex.demo.indexData.datas.service.IndexDataService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class IndexDataController {
+
+    private final IndexDataService indexDataService;
+
+    @GetMapping("api/index-data/{indexInfoId}/chart")
+    public ResponseEntity<List<IndexChartDto>> getIndexChart(
+        @PathVariable int indexInfoId,
+        @RequestParam(required = false, defaultValue = "DAILY") PeriodType periodType
+    ) {
+        List<IndexChartDto> indexChartDtos = indexDataService.getIndexChart(periodType,
+            indexInfoId);
+
+        return ResponseEntity.ok(indexChartDtos);
+    }
+
+    @PostMapping("api/index-data/performance/rank")
+    public ResponseEntity<List<RankedIndexPerformanceDto>> getIndexPerformanceRank(
+        @RequestParam(required = false, defaultValue = "DAILY") PeriodType periodType,
+        @RequestParam(required = false) Integer indexInfoId,
+        @RequestParam(required = false, defaultValue = "10") Integer limit
+    ) {
+        List<RankedIndexPerformanceDto> indexPerformanceRankDtos = indexDataService.getIndexPerformanceRank(
+            periodType, indexInfoId, limit);
+
+        return ResponseEntity.ok(indexPerformanceRankDtos);
+    }
+
+    @PostMapping("api/index-data/performance/favorite")
+    public ResponseEntity<List<IndexPerformanceDto>> getFavoriteIndexPerformanceRank(
+        @RequestParam(required = false, defaultValue = "DAILY") PeriodType periodType
+    ) {
+        List<IndexPerformanceDto> indexPerformanceDtos =
+            indexDataService.getFavoriteIndexPerformanceRank(periodType);
+
+        return ResponseEntity.ok(indexPerformanceDtos);
+    }
+}
