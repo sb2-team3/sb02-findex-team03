c79b12e2e36aaae66642e8ea84a030e4d010b229
leejunnyeop <daffodil8620@naver.com>
2025-04-23 01:01:17 +0900

    fix : 직렬화/역직렬화 에러
    -BaseTimeEntity에 JSON 직렬화/역직렬화 및 날짜 포맷 설정 추가
    - createdAt, updatedAt 필드에 LocalDate 직렬화/역직렬화 핸들러 적용
    - ErrorResponse.timestamp Type 변경

--- src/main/java/com/findex/demo/global/error/ErrorResponse.java
+++ src/main/java/com/findex/demo/global/error/ErrorResponse.java
@@ -1,18 +1,19 @@
 package com.findex.demo.global.error;
 
-import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import lombok.Getter;
 
 @Getter
 public class ErrorResponse {
 
-    private final Instant timestamp;
+    private final LocalDateTime timestamp;
     private final int status;
     private final String message;
     private final String details;
 
     public ErrorResponse(ErrorCode errorCode, String details) {
-        this.timestamp = Instant.now();
+        this.timestamp = LocalDateTime.now();
         this.status = errorCode.getStatus().value();
         this.message = errorCode.getMessage();
         this.details = details;
