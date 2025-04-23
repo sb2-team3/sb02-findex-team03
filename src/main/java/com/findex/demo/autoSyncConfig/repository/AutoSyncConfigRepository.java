1f6a290cfd897ec632137c9f50c14c90d45d0b09
leejunnyeop <daffodil8620@naver.com>
2025-04-23 11:11:06 +0900

    fix : 버전 업그레드 재수정

--- src/main/java/com/findex/demo/autoSyncConfig/repository/AutoSyncConfigRepository.java
+++ src/main/java/com/findex/demo/autoSyncConfig/repository/AutoSyncConfigRepository.java
@@ -2,6 +2,8 @@ package com.findex.demo.autoSyncConfig.repository;
 
 import com.findex.demo.autoSyncConfig.domain.entity.AutoSyncConfig;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AutoSyncConfigRepository extends JpaRepository<AutoSyncConfig, Integer> {
 }
