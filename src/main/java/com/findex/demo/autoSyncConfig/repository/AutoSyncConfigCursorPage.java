df4af80ea8fc9a9255a26fb21682f906d2fa2f26
leejunnyeop <daffodil8620@naver.com>
2025-04-24 00:20:07 +0900

    feat: AutoSyncConfig 커서 페이지네이션 인터페이스 및 실행 로직 구현
    - AutoSyncConfigCursorPage 인터페이스 정의 및 AutoSyncConfigCursorPageExecutor 구현

---
+++ src/main/java/com/findex/demo/autoSyncConfig/repository/AutoSyncConfigCursorPage.java
@@ -0,0 +1,11 @@
+package com.findex.demo.autoSyncConfig.repository;
+
+import com.findex.demo.autoSyncConfig.domain.entity.AutoSyncConfig;
+import com.findex.demo.global.pagination.dto.PagedResponse;
+import com.findex.demo.global.pagination.dto.SortDirection;
+import com.findex.demo.global.pagination.dto.SortField;
+
+public interface AutoSyncConfigCursorPage {
+    PagedResponse<AutoSyncConfig> getPage(Integer indexInfoId, Boolean enabled, int idAfter, String cursor,
+                                          SortField sortField, SortDirection sortDirection, int size);
+}
