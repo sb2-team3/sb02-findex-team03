c22e6d3d0287c911933807b3703d3d238f6b0f28
kitmtaewoo <bezzi1654@kookmin.ac.kr>
2025-04-24 18:20:34 +0900

    fix: index data repository에 메서드 추가

--- src/main/java/com/findex/demo/indexData/index/repository/IndexDataRepository.java
+++ src/main/java/com/findex/demo/indexData/index/repository/IndexDataRepository.java
@@ -5,7 +5,6 @@ import com.findex.demo.indexInfo.domain.entity.IndexInfo;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -25,51 +24,17 @@ public interface IndexDataRepository extends JpaRepository<IndexData, Integer>,
     List<IndexData> findByIndexInfoAndBaseDateBetweenOrderByBaseDateAsc(IndexInfo indexInfo,
         LocalDate startDate, LocalDate endDate);
 
-    @Query(
-        "SELECT i FROM IndexData i WHERE i.indexInfo.id = :favoriteIndexId AND i.baseDate BETWEEN :startDate "
-            + "AND :endDate")
-    IndexData findByIndexInfoIdAndBaseDateBetween(Integer favoriteIndexId,
-        LocalDate startDate, LocalDate endDate);
-
-    @Query(
-        "SELECT i FROM IndexData i WHERE i.indexInfo IN :indexInfoList AND i.baseDate BETWEEN :startDate AND :endDate")
-    List<IndexData> findByIndexInfoInAndBaseDateBetween(List<IndexInfo> indexInfoList,
-        LocalDate startDate, LocalDate endDate);
-
-
-    @Query("""
-        SELECT i FROM IndexData i
-        WHERE i.indexInfo.id = :indexInfoId
-        AND (:startDate IS NULL OR i.baseDate >= :startDate)
-        AND (:endDate IS NULL OR i.baseDate <= :endDate)
-        AND (:cursorId IS NULL OR i.id > :cursorId)
-        ORDER BY i.id ASC
-        """)
-    List<IndexData> findWithIndexInfoId(Integer indexInfoId, LocalDate startDate, LocalDate endDate,
-        Integer cursorId, Pageable pageable);
-
-    @Query("""
-        SELECT i FROM IndexData i
-        WHERE i.indexInfo.id = :indexInfoId
-        AND (:startDate IS NULL OR i.baseDate >= :startDate)
-        AND (:endDate IS NULL OR i.baseDate <= :endDate)
-        AND (:cursorId IS NULL OR i.id > :cursorId)
-        ORDER BY i.id ASC
-        """)
-    List<IndexData> findWithoutIndexInfoId(LocalDate startDate, LocalDate endDate, Integer cursorId,
-        Pageable pageable);
 
     List<IndexData> findByIndexInfoIdInAndBaseDateIn(List<Integer> favoriteIndexIds,
         List<LocalDate> startDate);
 
-    List<IndexData> findByIndexInfoInAndBaseDateIn(List<IndexInfo> indexInfoList,
-        List<LocalDate> localDates);
-
     boolean existsByBaseDate(LocalDate date);
 
-    Optional<LocalDate> findMaxBaseDateBeforeDate(LocalDate date);
+    @Query("SELECT MAX(i.baseDate) FROM IndexData i WHERE i.baseDate < :date")
+    Optional<LocalDate> findMaxBaseDateBeforeDate(@Param("date") LocalDate date);
 
-    Optional<LocalDate> findMinBaseDateAfterDate(LocalDate date);
+    @Query("SELECT MIN(i.baseDate) FROM IndexData i WHERE i.baseDate > :date")
+    Optional<LocalDate> findMinBaseDateAfterDate(@Param("date") LocalDate date);
 
     Optional<IndexData> findTopByIndexInfoAndBaseDateLessThanEqualOrderByBaseDateDesc(
         IndexInfo info, LocalDate date);
