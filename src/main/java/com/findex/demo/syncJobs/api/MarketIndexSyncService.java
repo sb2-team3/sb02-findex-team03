7f8290246f514b071a3db64d9c5e20bebf3c5677
KimSeeun <9035anny@naver.com>
2025-04-24 11:04:40 +0900

    refactor : 데이터개수 100으로 수정

--- src/main/java/com/findex/demo/syncJobs/api/MarketIndexSyncService.java
+++ src/main/java/com/findex/demo/syncJobs/api/MarketIndexSyncService.java
@@ -13,7 +13,6 @@ import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -35,7 +34,7 @@ public class MarketIndexSyncService {
     @Value("${external.market-index.num-of-rows:100}")
     private int numOfRows;
 
-    private static final int TOTAL_COUNT = 199169;
+    private static final int TOTAL_COUNT = 100;
     private static final int TOTAL_PAGES = (int) Math.ceil((double) TOTAL_COUNT / 100);
 
     public void fetchAndStoreMarketIndices() {
