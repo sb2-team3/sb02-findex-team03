b006bb8e01b4d17f00725625ffd81a3e020ff43c
kitmtaewoo <bezzi1654@kookmin.ac.kr>
2025-04-24 18:24:52 +0900

    fix: index data repository에 메서드 수정

--- src/main/java/com/findex/demo/indexData/index/repository/IndexDataRepository.java
+++ src/main/java/com/findex/demo/indexData/index/repository/IndexDataRepository.java
@@ -5,6 +5,7 @@ import com.findex.demo.indexInfo.domain.entity.IndexInfo;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -24,9 +25,31 @@ public interface IndexDataRepository extends JpaRepository<IndexData, Integer>,
     List<IndexData> findByIndexInfoAndBaseDateBetweenOrderByBaseDateAsc(IndexInfo indexInfo,
         LocalDate startDate, LocalDate endDate);
 
+    @Query("""
+        SELECT i FROM IndexData i
+        WHERE i.indexInfo.id = :indexInfoId
+        AND (:startDate IS NULL OR i.baseDate >= :startDate)
+        AND (:endDate IS NULL OR i.baseDate <= :endDate)
+        AND (:cursorId IS NULL OR i.id > :cursorId)
+        ORDER BY i.id ASC
+        """)
+    List<IndexData> findWithIndexInfoId(Integer indexInfoId, LocalDate startDate, LocalDate endDate,
+        Integer cursorId, Pageable pageable);
+
+    @Query("""
+        SELECT i FROM IndexData i
+        WHERE i.indexInfo.id = :indexInfoId
+        AND (:startDate IS NULL OR i.baseDate >= :startDate)
+        AND (:endDate IS NULL OR i.baseDate <= :endDate)
+        AND (:cursorId IS NULL OR i.id > :cursorId)
+        ORDER BY i.id ASC
+        """)
+    List<IndexData> findWithoutIndexInfoId(LocalDate startDate, LocalDate endDate, Integer cursorId,
+        Pageable pageable);
 
     List<IndexData> findByIndexInfoIdInAndBaseDateIn(List<Integer> favoriteIndexIds,
         List<LocalDate> startDate);
+
 
     boolean existsByBaseDate(LocalDate date);
 
