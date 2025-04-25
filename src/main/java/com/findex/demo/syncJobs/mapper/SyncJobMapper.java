325264da8141e1f7e600f36993c495a0649ba2af
leejunnyeop <daffodil8620@naver.com>
2025-04-25 22:18:08 +0900

    chore : SyncJob jobTime Type 변경
    - LocalDateTime -> LocalDate

--- src/main/java/com/findex/demo/syncJobs/mapper/SyncJobMapper.java
+++ src/main/java/com/findex/demo/syncJobs/mapper/SyncJobMapper.java
@@ -8,15 +8,13 @@ import java.time.ZoneId;
 public class SyncJobMapper {
 
   public static SyncJobDto toDto(SyncJob entity) {
-    if (entity == null) return null;
-
     return SyncJobDto.builder()
         .id(entity.getId())
         .jobType(entity.getJobType())
         .indexInfoId(entity.getIndexInfo().getId())
         .targetDate(entity.getTargetDate().atStartOfDay(ZoneId.systemDefault()).toInstant())
         .worker(entity.getWorker())
-        .jobTime(entity.getJobTime().atZone(ZoneId.systemDefault()).toInstant())
+        .jobTime(entity.getJobTime())
         .result(entity.getResult())
         .build();
   }
