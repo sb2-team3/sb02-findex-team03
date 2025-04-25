4a8198c8df1e279da9516a284df6e0e065b39d3c
KimSeeun <9035anny@naver.com>
2025-04-25 09:48:57 +0900

    refactor : swagger에 맞게 수정

--- src/main/java/com/findex/demo/syncJobs/controller/SyncJobController.java
+++ src/main/java/com/findex/demo/syncJobs/controller/SyncJobController.java
@@ -1,6 +1,6 @@
 package com.findex.demo.syncJobs.controller;
 
-import com.findex.demo.syncJobs.domain.dto.CursorPageResponse;
+import com.findex.demo.syncJobs.domain.dto.CursorPageResponseSyncJobDto;
 import com.findex.demo.syncJobs.domain.dto.SyncJobDto;
 import com.findex.demo.syncJobs.domain.dto.SyncJobSearchRequest;
 import com.findex.demo.syncJobs.service.SyncJobService;
@@ -15,7 +15,7 @@ public class SyncJobController {
   private final SyncJobService syncJobService;
 
   @GetMapping
-  public CursorPageResponse<SyncJobDto> searchSyncJobs(
+  public CursorPageResponseSyncJobDto<SyncJobDto> searchSyncJobs(
       @ModelAttribute SyncJobSearchRequest request,
       @RequestParam(defaultValue = "jobTime") String sortField,
       @RequestParam(defaultValue = "desc") String sortDirection,
